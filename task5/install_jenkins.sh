df31f5115d20e41b55fb0399a6c6a49aa03cf298
geremygustavo <abelmallcu123@gmail.com>
2019-02-21 14:00:53 -0400

    change environment to yml file

--- task5/install_jenkins.sh
+++ task5/install_jenkins.sh
@@ -5,4 +5,4 @@ sudo apt update
 sudo apt install openjdk-11-jdk -y
 
 #install jenkins:2.107.3-alpine
-docker pull jenkins/jenkins:lts
+docker pull registry.jala.info/devops/jenkins/jenkins:2.150.3-alpine
