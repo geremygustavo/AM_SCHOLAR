576afcf1ae1aa77bd371165e2ee553daaac69882
geremygustavo <abelmallcu123@gmail.com>
2019-02-25 14:22:54 -0400

    resolve conflicts

--- task7/install_jenkins.sh
+++ task7/install_jenkins.sh
@@ -1,6 +1,2 @@
-#install jenkins:2.107.3-alpine
-docker pull registry.jala.info/devops/jenkins/jenkins:2.150.3-alpine
-
 mkdir -p docker/jenkins_home
-cd docker
-chmod 777 jenkins_home
+Chmod 777 docker / jenkins_home
